48967767a7f976f88f00ea58f7e8827b1401d283
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-26 17:21:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+ document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const age = document.querySelector('#age').value;
+    const name = document.querySelector('#name').value;
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (age >= 18) {
+          resolve();
+        } else {
+          reject();
+        }
+      }, 4000);
+    }).then(() => {
+      alert(`Welcome, ${name}. You can vote.`);
+    }).catch(() => {
+      alert(`Oh sorry ${name}. You aren't old enough.`);
+    });
+  });
